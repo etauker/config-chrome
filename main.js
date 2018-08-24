7acbe52ffec358018420289a9776b0d60f99ad50
Tautvydas Kersulis <etauker@gmail.com>
2018-08-24 22:46:17 +0100

    Added use of the helper

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const { execSync } = require('child_process');
+let helper = require('../development-setup/lib/helper.js');
+
 
 var _executeCommand = function(sCommand, bDebug) {
     if (bDebug) console.log(`[debug] Executing command: ${sCommand}`);
@@ -16,17 +18,17 @@ var _executeCommand = function(sCommand, bDebug) {
 };
 
 var install = function(oConfig) {
-    console.log("Starting config-chrome setup - running install");
-    process.chdir(oConfig.workspace + "/config-chrome");
-    _executeCommand(`bash install.sh --os=${oConfig.platform}`, oConfig.options.debug);
+    console.log("-- [config-atom] - running install");
+    helper.changeDirectory(oConfig.platform, oConfig.workspace, "config-chrome", oConfig.options.debug);
+    helper.executeCommand(`bash install.sh --os=${oConfig.platform}`, oConfig.options.debug);
 };
 
 var configure = function(oConfig) {
-    console.log("config-chrome setup - configure not implemented");
+    console.log("-- [config-chrome] - configure not implemented");
 };
 
 var backup = function(oConfig) {
-    console.log("config-chrome setup - backup not implemented");
+    console.log("-- [config-chrome] - backup not implemented");
 };
 
 module.exports.install = install;
