9da98fe73c1d4fc8b244a39f662779d664c21f20
Tautvydas Kersulis <etauker@gmail.com>
2018-08-24 22:48:42 +0100

    Added use of the helper.

--- main.js
+++ main.js
@@ -1,32 +1,18 @@
 const { execSync } = require('child_process');
-
-var _executeCommand = function(sCommand, bDebug) {
-    if (bDebug) console.log(`[debug] Executing command: ${sCommand}`);
-    try {
-        var stdout = execSync(sCommand);
-        console.log("Command Output: ");
-        console.log(stdout.toString('utf8'));
-    } catch (stderr) {
-        console.log("--------------------------------------------------");
-        console.log("Error executing command: " + sCommand);
-        console.log("--------------------------------------------------");
-        console.log("stderr: " + stderr);
-        console.log("--------------------------------------------------");
-    }
-};
+let helper = require('../development-setup/lib/helper.js');
 
 var install = function(oConfig) {
-    console.log("Starting config-chrome setup - running install");
-    process.chdir(oConfig.workspace + "/config-chrome");
-    _executeCommand(`bash install.sh --os=${oConfig.platform}`, oConfig.options.debug);
+    console.log("-- [config-atom] - running install");
+    helper.changeDirectory(oConfig.platform, oConfig.workspace, "config-chrome", oConfig.options.debug);
+    helper.executeCommand(`bash install.sh --os=${oConfig.platform}`, oConfig.options.debug);
 };
 
 var configure = function(oConfig) {
-    console.log("config-chrome setup - configure not implemented");
+    console.log("-- [config-chrome] - configure not implemented");
 };
 
 var backup = function(oConfig) {
-    console.log("config-chrome setup - backup not implemented");
+    console.log("-- [config-chrome] - backup not implemented");
 };
 
 module.exports.install = install;
